6e73c652008236f49af230172b337a96d65a0cb9
Saturn226 <serenehopes@gmail.com>
2016-10-16 23:02:19 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,29 @@
+
 describe('Favorite Food Filter', function () {
-	var $controller;
+  var $controller;
 
-	beforeEach(module('app'));
+  beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($injector) {
+    $filter = $injector.get('$filter');
+  }));
 
-	
+  it('should filter favorite food correctly', function(){
+    var mockedList =[{
+      name: "Ashley",
+      favoriteFood: "Pizza"
+    },{
+      name: "Lee",
+      favoriteFood: "Fish"
+    },{
+      name: "Avi",
+      favoriteFood: "Burgers"
+    }];
+
+    var results = $filter('favoriteFood')(mockedList, 'Fish');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe("Lee");
+  })
+  
 });
